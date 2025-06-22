094afcfeee7a43c8757bd299b01e20aab387f873
Yonathan Kurin <yonatank92@gmail.com>
2025-06-22 21:11:09 +0300

    update

--- weather-web/src/store/reducers/alertReducer.ts
+++ weather-web/src/store/reducers/alertReducer.ts
@@ -15,7 +15,8 @@ export type Alert = {
 };
 
 // --- API Base URL from .env ---
-const API_BASE_URL = process.env.REACT_APP_ALERT_SERVICE_BASE_URL;
+const API_BASE_URL =
+  process.env.REACT_APP_ALERT_SERVICE_BASE_URL || "http://localhost:3001";
 
 // --- Initial State ---
 const initialState = {
--- weather_api/src/index.ts
+++ weather_api/src/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
